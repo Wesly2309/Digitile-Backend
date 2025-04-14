3f02c296a2cbd2177d34793d44f45c680d01a772
Wesly2309 <weslylims02@gmail.com>
2025-04-14 13:42:36 +0700

    feat: fixing bugs

--- api/index.js
+++ api/index.js
@@ -1,35 +1,10 @@
-const express = require('express');
-const serverless = require('serverless-http');
+const app = require('../src/index');
 const dotenv = require('dotenv');
-const authRoutes = require('../src/routes/auth_routes'); 
-const userRoutes = require('../src/routes/user_routes');
-const capsuleRoutes = require('../src/routes/capsule_routes');
-const geolocationRoutes = require('../src/routes/geolocation_route');
-const logsRoute = require('../src/routes/logs_route');
-const missionRoutes = require('../src/routes/mission_routes');
-const bodyParser = require('body-parser');
-const cors = require('cors');
 
-// Load konfigurasi .env
 dotenv.config();
 
-const app = express();
+const PORT = process.env.SERVER_PORT || 3000;
 
-// Middleware
-app.use(cors());
-app.use(express.json());
-app.use(bodyParser.urlencoded({ extended: true }));
-
-app.get('/', (req, res) => {
-    res.send('Hello World');
-  });
-
-// Definisikan rute-rute Anda
-app.use('/auth', authRoutes);
-app.use('/user', userRoutes);
-app.use('/capsule', capsuleRoutes);
-app.use('/geo-location', geolocationRoutes);
-app.use('/logs', logsRoute);
-app.use('/mission', missionRoutes);
-
-module.exports.handler = serverless(app);
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
---
+++ src/index.js
@@ -0,0 +1,37 @@
+const express = require('express');
+const dotenv = require('dotenv');
+const authRoutes = require('../src/routes/auth_routes'); 
+const userRoutes = require('../src/routes/user_routes');
+const capsuleRoutes = require('../src/routes/capsule_routes');
+const geolocationRoutes = require('../src/routes/geolocation_route');
+const logsRoute = require('../src/routes/logs_route');
+const missionRoutes = require('../src/routes/mission_routes');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+
+// Load konfigurasi .env
+dotenv.config();
+
+const app = express();
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get('/', (req, res) => {
+    res.send('Hello World');
+  });
+
+// Definisikan rute-rute Anda
+app.use('/auth', authRoutes);
+app.use('/user', userRoutes);
+app.use('/capsule', capsuleRoutes);
+app.use('/geo-location', geolocationRoutes);
+app.use('/logs', logsRoute);
+app.use('/mission', missionRoutes);
+
+
+
+
+module.exports = app
