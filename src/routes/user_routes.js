08a7a09d88dd6bc2d63c3f2b3c4fec4c81064a6d
Wesly2309 <weslylims02@gmail.com>
2025-04-14 09:32:00 +0700

    feat: the last

--- src/routes/user_routes.js
+++ src/routes/user_routes.js
@@ -1,11 +1,10 @@
 const express = require('express')
-const {getUserByToken , getUserById, updateProfile, deleteUser} = require('../controllers/user_controller')
+const {getUserByToken , getUserById, updateProfile} = require('../controllers/user_controller')
 const router = express.Router()
 
 
 router.get('/', getUserByToken)
 router.get('/:id'  , getUserById)
 router.put('/update' , updateProfile)
-router.delete('/delete/:id' , deleteUser)
 
 module.exports = router
